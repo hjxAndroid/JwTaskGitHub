ff0a42f75ac6076c3ca383ff47e68770d105af92
Ragn <lrk123456@hotmail.com>
2015-11-30 10:39:20 +0800

    Merge branch 'master' of https://git.oschina.net/yilongmd/JwTask
    # Conflicts:
    #	app/src/main/res/values/dimens.xml

---
+++ app/src/main/java/com/jeeweel/syl/jwtask/business/main/module/task/SignUpActivity.java
@@ -0,0 +1,28 @@
+package com.jeeweel.syl.jwtask.business.main.module.task;
+
+import android.os.Bundle;
+
+import com.jeeweel.syl.jwtask.R;
+import com.jeeweel.syl.jwtask.business.main.module.more.MineActivity;
+import com.jeeweel.syl.lib.api.core.activity.baseactivity.JwActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * Created by Ragn on 2015/11/27.
+ */
+public class SignUpActivity extends JwActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_sign_up);
+        ButterKnife.bind(this);
+        setTitle("发起签到");
+    }
+
+    @OnClick(R.id.line_start_sign_user)
+    void personlInformation() {
+        JwStartActivity(MineActivity.class);
+    }
+}
